ba407056f3441574f79c7f41046ee93e52a5cde5
Brandon <brandongines@gmail.com>
2019-07-15 17:34:26 -0400

    first

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,19 +67,7 @@ module.exports = {
 				background_color: config.backgroundColor,
 				theme_color: config.themeColor,
 				display: 'standalone',
-				icon: config.favicon,
-				icons: [
-					{
-						src: '/icons/icon-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: '/icons/icon-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				]
+				icon: config.favicon
 			}
 		},
 		'gatsby-plugin-offline',
