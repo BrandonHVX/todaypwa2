1007997e6971743ff599dae55561446324a7558c
Brandon <brandongines@gmail.com>
2019-07-15 17:26:09 -0400

    first

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,76 +1,88 @@
-const config = require('./config')
+const config = require('./config');
 
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
 
 module.exports = {
-  pathPrefix: config.pathPrefix,
-  siteMetadata: {
-    siteUrl: config.siteUrl + pathPrefix,
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'post',
-        path: `${__dirname}/blog`,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: config.googleAnalyticsID,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-mdx',
-      options: {
-        gatsbyRemarkPlugins: [
-          {
-            resolve: 'gatsby-remark-external-links',
-            options: {
-              target: '_blank',
-              rel: 'nofollow noopener noreferrer',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 830,
-              quality: 90,
-              withWebp: true,
-              linkImagesToOriginal: false,
-            },
-          },
-          // TODO: Replace with "mdx-component-autolink-headers"
-          {
-            resolve: 'gatsby-remark-autolink-headers',
-            options: {
-              maintainCase: false,
-            },
-          },
-        ],
-      },
-    },
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-lodash',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: config.siteTitleAlt,
-        short_name: config.siteTitleManifest,
-        description: config.siteDescription,
-        start_url: config.pathPrefix,
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: 'standalone',
-        icon: config.favicon,
-      },
-    },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-netlify',
-  ],
-}
+	pathPrefix: config.pathPrefix,
+	siteMetadata: {
+		siteUrl: config.siteUrl + pathPrefix
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-styled-components',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'post',
+				path: `${__dirname}/blog`
+			}
+		},
+		{
+			resolve: 'gatsby-plugin-google-analytics',
+			options: {
+				trackingId: config.googleAnalyticsID
+			}
+		},
+		{
+			resolve: 'gatsby-plugin-mdx',
+			options: {
+				gatsbyRemarkPlugins: [
+					{
+						resolve: 'gatsby-remark-external-links',
+						options: {
+							target: '_blank',
+							rel: 'nofollow noopener noreferrer'
+						}
+					},
+					{
+						resolve: 'gatsby-remark-images',
+						options: {
+							maxWidth: 830,
+							quality: 90,
+							withWebp: true,
+							linkImagesToOriginal: false
+						}
+					},
+					// TODO: Replace with "mdx-component-autolink-headers"
+					{
+						resolve: 'gatsby-remark-autolink-headers',
+						options: {
+							maintainCase: false
+						}
+					}
+				]
+			}
+		},
+		'gatsby-plugin-catch-links',
+		'gatsby-plugin-sitemap',
+		'gatsby-plugin-lodash',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: config.siteTitleAlt,
+				short_name: config.siteTitleManifest,
+				description: config.siteDescription,
+				start_url: config.pathPrefix,
+				background_color: config.backgroundColor,
+				theme_color: config.themeColor,
+				display: 'standalone',
+				icon: config.favicon,
+				icons: [
+					{
+						src: '/public/icons/icon-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/public/icons/icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		},
+		'gatsby-plugin-offline',
+		'gatsby-plugin-netlify'
+	]
+};
